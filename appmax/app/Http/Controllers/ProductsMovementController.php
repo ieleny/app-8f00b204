0cd043ecdb57df500894ecd57053b83c6e884f51
ieleny <ielenyfilgueira@hotmail.com>
2021-10-08 19:42:40 -0300

    Adicionado a classe ProductsMovementController

--- appmax/app/Http/Controllers/ProductsMovementController.php
+++ appmax/app/Http/Controllers/ProductsMovementController.php
@@ -4,7 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\ProductsMovementModel;
+
 class ProductsMovementController extends Controller
 {
-    //
+    public function saveProductsMovement($product): ProductsMovementModel
+    {
+        $productsMovement = new ProductsMovementModel;
+        $productsMovement->products_id = $product->id;
+        $productsMovement->products_movement_quantity = $product->products_quantity;
+        $productsMovement->products_sku = $product->products_sku;
+
+        return $productsMovement;
+    }
 }
