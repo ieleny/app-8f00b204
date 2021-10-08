f41822350b1d77b953d275803b0c493fb48be2a0
ieleny <ielenyfilgueira@hotmail.com>
2021-10-07 23:33:36 -0300

    Migrations das tabelas products, products_movement e logs

---
+++ appmax/database/migrations/2021_10_07_231428_products.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Products extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->bigInteger('products_sku')->unique();
+            $table->string('product_name');
+            $table->float('product_quantity', 10,3);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('products');
+    }
+}
---
+++ appmax/database/migrations/2021_10_07_232259_products_movement.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ProductsMovement extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products_movement', function (Blueprint $table) {
+            $table->integer('products_sku');
+            $table->float('products_movement_quantity', 10,3);
+            $table->foreignId('products_id')->constrained('products');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('products_movement');
+    }
+}
