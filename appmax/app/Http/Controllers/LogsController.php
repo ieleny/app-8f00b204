cd545f05b3caae773987feff72023dc596d10a8a
ieleny <ielenyfilgueira@hotmail.com>
2021-10-08 19:41:33 -0300

    Adicionado a função saveLog

--- appmax/app/Http/Controllers/LogsController.php
+++ appmax/app/Http/Controllers/LogsController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\LogsModel;
 
 class LogsController extends Controller
 {
-    //
+
+    public function saveLog($product, $action):LogsModel
+    {
+        $logs = new LogsModel;
+        $logs->products_sku = $product->products_sku;
+        $logs->products_quantity = $product->products_quantity;
+        $logs->logs_action = $action;
+
+        return $logs;
+    }
 }
