c6be9206a7dc8510e51e133455c65a1070cdc790
ieleny <ielenyfilgueira@hotmail.com>
2021-10-08 23:42:56 -0300

    retirado o update do produto

--- appmax/app/Http/Controllers/ProductsController.php
+++ appmax/app/Http/Controllers/ProductsController.php
@@ -52,114 +52,6 @@ class ProductsController extends Controller
         ], 201);
     }
 
-    public function update($id, Request $request)
-    {   
-        $product = ProductsModel::find($id);
-        $mensagemValidation = $this->validateUpdate($id, $request, $product);
-
-        if($mensagemValidation == 'success')
-        {   
-            if(!is_null($request->quantity) && !is_null($request->type_movimentacion)){
-                $product->products_quantity = $this->quantityStock(
-                    $request->quantity, 
-                    $product->products_quantity,
-                    $request->type_movimentacion
-                );
-        
-            }else {
-                $product->products_quantity = $product->products_quantity;
-            }
-
-            $product->products_name = is_null($request->name) 
-                    ? $product->products_name 
-                    : $request->name;
-
-            $product->save();
-
-            $this->saveHistoric($product, 'UPDATELOG');
-
-            return response()->json([
-                "mensagem" => "Produto foi atualizado com sucesso!"
-            ], 201);
-        }
-
-        return response()->json([
-            "mensagem" => $mensagemValidation
-        ], 404);;
-    }
-
-    private function validateUpdate($id, $request, $product): string
-    {
-        $mensagem = 'success';
-        $validation = false;
-
-        if(!ProductsModel::where('id', $id)->exists()){
-            $mensagem = "Produto nao encontrado!";
-            $validation = true;
-        }
-
-        if(!is_null($request->quantity) && is_null($request->type_movimentacion) && $validation == false){
-            $mensagem = "É preciso informar qual tipo de operação no estoque, ADICAO=A ou REMOCAO=R!";
-        }
-
-        if(!is_null($request->quantity) && !is_null($request->type_movimentacion) && $validation == false){
-            $verifiedQuantity = $this->verifiedQuantity(
-                                    $request->quantity, 
-                                    $product->products_quantity,
-                                    $request->type_movimentacion
-                                );
-
-            if($verifiedQuantity !== 'success'){
-                return $verifiedQuantity;
-            } 
-
-        }
-
-        return $mensagem;
-    }
-
-    private function verifiedQuantity($quantityRequest, $quantityDataBase, $typeMovimentacion): string
-    {   
-        $mensagem = 'success';
-        $isNumeric = is_numeric($quantityRequest);
-
-        if(!$isNumeric)
-        {
-            $mensagem = "A Quantidade precisa ser um decimal ou inteiro!";
-
-        }else if($isNumeric && !is_null($quantityRequest)){
-
-            $quantity = $this->quantityStock(
-                        $quantityRequest, 
-                        $quantityDataBase, 
-                        $typeMovimentacion
-            );
-
-            if($quantity < 0)
-            {
-                $mensagem =  "O estoque nao pode ficar negativo!";
-            }             
-        }
-        
-        return $mensagem;
-    }
-
-    private function quantityStock($quantityRequest, $quantityDataBase, $typeMovimentacion): float
-    {
-        $quantity = 0.0;
-
-        if($typeMovimentacion == 'A')
-        {   
-            $quantity = (float)$quantityRequest + (float)$quantityDataBase;
-        } 
-        else if($typeMovimentacion == 'R')
-        {
-            $quantity =  (float)$quantityDataBase - (float)$quantityRequest;
-        }
-
-        return (float)$quantity;
-    }
-
     private function saveProducts($request): ProductsModel
     {
         $product = new ProductsModel;
@@ -181,7 +73,6 @@ class ProductsController extends Controller
         $logs = $this->logsController
                 ->saveLog($product, $keyLog);
         $logs->save();
-
     }
 
 }
